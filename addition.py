36c597e495a7e099453c20a916e52fff2d4f7f30
Verina Cristie <cristie_verina@sutd.edu.sg>
2019-05-09 16:57:51 +0800

    updating subtraction branch

--- addition.py
+++ addition.py
@@ -1,7 +1,10 @@
 a = int(input("enter first number: "))
 b = int(input("enter second number: "))
 c = int(input("enter third number: "))
- 
+d = int(input("subtracted by: "))
+
 sum = a + b + c
- 
+subtraction = sum - d 
+
 print("sum:", sum)
+print("subtraction result", subtraction)
